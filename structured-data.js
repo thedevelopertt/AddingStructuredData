491844d0ca6d2e404ba38d290040f8eed548c8d9
Greg <greigdillon@gmail.com>
2019-08-30 17:58:10 -0500

    Initial Commit : Added Structured Data to index.html

---
+++ structured-data.js
@@ -0,0 +1,31 @@
+<script type = "application/ld+json" > [{
+  "@context": "http://schema.org",
+  "@type": "WebSite",
+  "url": "<URL HomePage>",
+  "name": "<Article title>",
+  "alternateName": "<Website Name>"
+}, {
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [{
+    "@type": "ListItem",
+    "position": 1,
+    "item": {
+      "@id": "<Article URL>",
+      "name": "<Article Title>",
+      "image": "<URL To Image>"
+    }
+  }]
+}, {
+  "@context": "http://schema.org",
+  "@type": "BlogPosting",
+  "url": "<URL HomePage>",
+  "name": "<Article Title>",
+  "alternateName": "<Website Name>",
+  "headline": "<Article Title>",
+  "image": {
+    "@type": "ImageObject",
+    "url": "<URL To Image>"
+  },
+  "description": "<Article Description>"
+}] </script>
